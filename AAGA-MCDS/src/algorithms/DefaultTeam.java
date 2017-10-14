13e8b2be32bb4c0fa2259240f81449f5dd76d2cd
Alexandre Fernandez <alexfer@openmailbox.org>
2017-10-14 14:25:09 +0200

    tests1+isvalide

--- AAGA-MCDS/src/algorithms/DefaultTeam.java
+++ AAGA-MCDS/src/algorithms/DefaultTeam.java
@@ -22,47 +22,48 @@ public class DefaultTeam {
 	Object[] ret5000 = test(1, 5000, 50);
 	Object[] ret10000 = test(1, 10000, 50);
 
-	Object[] ret2100 = test(2, 100, 5);
-	Object[] ret2500 = test(2, 500, 25);
-	Object[] ret21000 = test(2, 1000, 50);
-	Object[] ret25000 = test(2, 5000, 250);
-	Object[] ret210000 = test(2, 10000, 500);
+//	Object[] ret2100 = test(2, 100, 5);
+//	Object[] ret2500 = test(2, 500, 25);
+//	Object[] ret21000 = test(2, 1000, 50);
+//	Object[] ret25000 = test(2, 5000, 250);
+//	Object[] ret210000 = test(2, 10000, 500);
 
 	System.out.println(
-		"1 - 100 points - Average size : " + ret100[0] + " points - Average time : " + ret100[1] + " s");
+		"1 - 100 points - Average size : " + ret100[0] + " points - Average time : " + ret100[1] + " s - Fails : " + ret100[2]);
 
 	System.out.println(
-		"1 - 500 points - Average size : " + ret500[0] + " points - Average time : " + ret500[1] + " s");
+		"1 - 500 points - Average size : " + ret500[0] + " points - Average time : " + ret500[1] + " s - Fails : " + ret500[2]);
 
 	System.out.println(
-		"1 - 1000 points - Average size : " + ret1000[0] + " points - Average time : " + ret1000[1] + " s");
+		"1 - 1000 points - Average size : " + ret1000[0] + " points - Average time : " + ret1000[1] + " s - Fails : " + ret1000[2]);
 
 	System.out.println(
-		"1 - 5000 points - Average size : " + ret5000[0] + " points - Average time : " + ret5000[1] + " s");
+		"1 - 5000 points - Average size : " + ret5000[0] + " points - Average time : " + ret5000[1] + " s - Fails : " + ret5000[2]);
 
 	System.out.println(
-		"1 - 10000 points - Average size : " + ret10000[0] + " points - Average time : " + ret10000[1] + " s");
+		"1 - 10000 points - Average size : " + ret10000[0] + " points - Average time : " + ret10000[1] + " s - Fails : " + ret10000[2]);
 
-	System.out.println(
-		"2 - 100 points - Average size : " + ret2100[0] + " points - Average time : " + ret2100[1] + " s");
-
-	System.out.println(
-		"2 - 500 points - Average size : " + ret2500[0] + " points - Average time : " + ret2500[1] + " s");
-
-	System.out.println(
-		"2 - 1000 points - Average size : " + ret21000[0] + " points - Average time : " + ret21000[1] + " s");
-
-	System.out.println(
-		"2 - 5000 points - Average size : " + ret25000[0] + " points - Average time : " + ret25000[1] + " s");
-
-	System.out.println("2 - 10000 points - Average size : " + ret210000[0] + " points - Average time : "
-		+ ret210000[1] + " s");
+//	System.out.println(
+//		"2 - 100 points - Average size : " + ret2100[0] + " points - Average time : " + ret2100[1] + " s");
+//
+//	System.out.println(
+//		"2 - 500 points - Average size : " + ret2500[0] + " points - Average time : " + ret2500[1] + " s");
+//
+//	System.out.println(
+//		"2 - 1000 points - Average size : " + ret21000[0] + " points - Average time : " + ret21000[1] + " s");
+//
+//	System.out.println(
+//		"2 - 5000 points - Average size : " + ret25000[0] + " points - Average time : " + ret25000[1] + " s");
+//
+//	System.out.println("2 - 10000 points - Average size : " + ret210000[0] + " points - Average time : "
+//		+ ret210000[1] + " s");
     }
 
     public static Object[] test(int id, int nb, int edgeTreshold) {
 	DefaultTeam d = new DefaultTeam();
 	int[] sizes = new int[100];
 	double[] times = new double[100];
+	int fails = 0;
 	for (int i = 0; i < 100; i++) {
 	    System.out.println("Test " + i);
 	    ArrayList<Point> points = d.readFromFile("tests" + id + "-" + nb + "/test" + (i + 1) + ".points");
@@ -71,7 +72,9 @@ public class DefaultTeam {
 	    long time2 = System.currentTimeMillis();
 	    times[i] = (time2 - time1);
 	    sizes[i] = pts.size();
-	    System.out.println("END - size : " + pts.size() + " - time : " + times[i] / 1000 + " s");
+	    boolean valid = isValid(points, pts, edgeTreshold);
+	    System.out.println("END - size : " + pts.size() + " - time : " + times[i] / 1000 + " s - valid : " + valid);
+	    if(!valid) fails++;
 	}
 	int avsize = 0;
 	for (int s : sizes)
@@ -82,10 +85,63 @@ public class DefaultTeam {
 	    avtime += t;
 	avtime /= 100.0 * 1000.0;
 	System.out.println("Average size : " + avsize + " points - Average time : " + avtime + " s");
-	return new Object[] { avsize, avtime };
+	return new Object[] { avsize, avtime, fails };
     }
 
-    public ArrayList<Point> calculConnectedDominatingSet(ArrayList<Point> points, int edgeThreshold) {
+    private static boolean isValid(ArrayList<Point> points, ArrayList<Point> pts, int edgeTreshold) {
+    	ArrayList<VertexDS> graphcds = new ArrayList<VertexDS>();
+    	boolean ret = true;
+    	for (int i = 0; i < pts.size(); i++) {
+    		VertexDS v = new VertexDS(pts.get(i), i);
+    	    graphcds.add(v);
+    	    v.makeBBComp();
+    	}
+    	for (VertexDS v : graphcds) {
+    	    for (VertexDS v2 : graphcds) {
+    		if (v == v2)
+    		    continue;
+    		if (v.p.distance(v2.p) < edgeTreshold) {
+    		    v.neighbors.add(v2);
+    		}
+    	    }
+    	}
+    	
+    	for(VertexDS v : graphcds) {
+    		for(VertexDS vn : v.neighbors) {
+    			v.bbcomp.union(vn.bbcomp);
+    		}
+    	}
+    	
+    	HashSet<DisjointSetElement<VertexDS>> compconnex = new HashSet<DisjointSetElement<VertexDS>>();
+    	for(VertexDS v : graphcds) {
+    		compconnex.add(v.bbcomp.find());
+    	}
+    	if(compconnex.size() > 1) {
+    		System.err.println("Error connexity : " + compconnex.size());
+    		ret = false;
+    	}
+    	ArrayList<Point> rest = (ArrayList<Point>) points.clone();
+    	for(int i=0; i<rest.size(); ) {
+    		boolean removed = false;
+    		Point p = rest.get(i);
+    		for(Point p1 : pts) {
+    			if(p1.distance(p) < edgeTreshold) {
+    				rest.remove(p);
+    				removed = true;
+    				break;
+    			}
+    		}
+    		if(!removed) i++;
+    	}
+    	if(rest.size() > 0) {
+    		System.err.println("Error dominating : " + rest.size());
+    		ret = false;
+    	}
+    	
+		return ret;
+	}
+
+	public ArrayList<Point> calculConnectedDominatingSet(ArrayList<Point> points, int edgeTreshold) {
 
 	ArrayList<VertexDS> graph = new ArrayList<VertexDS>();
 
@@ -96,7 +152,7 @@ public class DefaultTeam {
 	    for (VertexDS v2 : graph) {
 		if (v == v2)
 		    continue;
-		if (v.p.distance(v2.p) < edgeThreshold) {
+		if (v.p.distance(v2.p) < edgeTreshold) {
 		    v.neighbors.add(v2);
 		}
 	    }
@@ -115,7 +171,7 @@ public class DefaultTeam {
 
 
 	MIS = distributedMIS(graph);
-	System.out.println(isMIS(graph, MIS));
+//	System.out.println(isMIS(graph, MIS));
 
 	// BLACK COMPONENTS
 	for (VertexDS vi : MIS) {
@@ -184,6 +240,7 @@ public class DefaultTeam {
 	    if (v.color.equals(Color.BLACK))
 		result.add(v.p);
 	}
+	System.out.println(isValid(points, result, edgeTreshold));
 	return result;
     }
 
